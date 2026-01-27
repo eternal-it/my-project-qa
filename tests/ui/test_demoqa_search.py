8c3e5989d76f874903908293b6f664607701bfc3
eternal-it <149970632+eternal-it@users.noreply.github.com>
2026-01-27 18:24:02 +0300

    0.0.16

--- tests/ui/test_demoqa_search.py
+++ tests/ui/test_demoqa_search.py
@@ -1,7 +1,8 @@
 # tests/ui/test_demoga_search.py
 
 def test_demoqa_search(page):
-    page.goto("https://the-internet.herokuapp.com")
+    page.goto("https://demoqa.com/text-box")
+    page.wait_for_selector("#userName", timeout=15000)
     page.fill("#userName", "Alex")
     page.fill("#userEmail", "alex@example.com")
     page.click("#submit")

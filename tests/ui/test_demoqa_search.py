bf74b1a280e9826229994206be9c59bb9ce81694
eternal-it <149970632+eternal-it@users.noreply.github.com>
2026-01-28 16:19:12 +0300

    0.0.22

--- tests/ui/test_demoqa_search.py
+++ tests/ui/test_demoqa_search.py
@@ -2,17 +2,17 @@
 
 import pytest
 
-@pytest.mark.flaky(reruns=2, reruns_delay=1)
+@pytest.mark.flaky(reruns=2, reruns_delay=1)     # Декоратор, помечает тест как нестабильный ("flaky") и пробует запустить его еще 2 раза, если упадет. между попытками 1 секунда
 def test_demoqa_search(page):
-    page.goto("https://demoqa.com/text-box")
+    page.goto("https://demoqa.com/text-box")     # Открывает страницу 
     
     # Ждём, пока поле станет кликабельным
-    page.wait_for_selector("#userName", state="visible", timeout=15000)
-    page.wait_for_function("() => document.querySelector('#userName').disabled === false")
+    page.wait_for_selector("#userName", state="visible", timeout=15000)       # Ждем пока userName станет активным, без этого page.fill может упасть
+    page.wait_for_function("() => document.querySelector('#userName').disabled === false") # Проверяем, что объект не заблокирован, отправляя java скрипт и ждем пока он вернет true 
     
-    page.fill("#userName", "Alex")
-    page.fill("#userEmail", "alex@example.com")
-    page.click("#submit")
+    page.fill("#userName", "Alex")              # Находит поля и вводит туда инфу
+    page.fill("#userEmail", "alex@example.com") 
+    page.click("#submit")         # Кликает по полю submit
     
     # Ждём результата
     page.wait_for_selector("#name", timeout=10000)
